563cc3ffba4b8e6526b019420faf64eedff72366
Piotr Zawadzki <piotr.zawadzki0@tlen.pl>
2024-11-22 21:30:42 +0100

    added s-z

---
+++ 0-10/u2dArrayList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class u2dArrayList {
+
+	public static void main(String[] args) {
+			
+		ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+
+		ArrayList<String> bakeryList = new ArrayList<>();
+		bakeryList.add("pasta");
+		bakeryList.add("bread");
+		bakeryList.add("donuts");
+
+		ArrayList<String> produceList = new ArrayList<>();
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
+
+		ArrayList<String> drinksList = new ArrayList<>();
+		drinksList.add("soda");
+		drinksList.add("coffe");
+		
+
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinksList);
+
+		//System.out.println(bakeryList);
+		//System.out.println(bakeryList.get(0));
+
+		//System.out.println(groceryList);
+		//System.out.println(groceryList.get(0)); // first list
+		System.out.println(groceryList.get(0).get(0)); // first list first element
+}
+}
+
---
+++ 0-10/zJavaMethods.java
@@ -0,0 +1,37 @@
+public class zJavaMethods {
+
+    //  method = a block of code that is executed whenever it is called upon
+
+    // public static void main(String[] args) {
+
+    
+
+    //     String name = "Adam";
+    //     int age = 21;
+    //     hello(name,age);
+
+    // }
+
+    // static void hello(String name, int age){
+    //     System.out.println("Hello " + name + " you are " + age);
+    // }
+
+    public static void main(String[] args) {
+        
+        int x = 3;
+        int y = 4;
+
+        //int z = add(x,y);
+        //System.out.println(z);
+
+        System.out.println(add(x,y));
+
+    }
+
+    static int add(int x, int y) {
+        
+        //int z = x + y;
+        return x + y;
+
+    }
+}
