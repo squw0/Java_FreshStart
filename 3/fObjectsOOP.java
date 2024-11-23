e34839980aa7819406f991f8ce4c04d403e3eb1e
Piotr Zawadzki <piotr.zawadzki0@tlen.pl>
2024-11-23 17:32:14 +0100

    fixes

--- 3/fObjectsOOP.java
+++ 3/fObjectsOOP.java
@@ -3,6 +3,17 @@ public class fObjectsOOP {
         
     // object = an instance of a class that may contain attributes and methods
     // example: (phone, desk, computer, cup)
-    
+
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+
+        System.out.println(myCar1.color);
+        System.out.println(myCar1.model);
+        System.out.println();
+        System.out.println(myCar2.color);
+        System.out.println(myCar2.model);
+        
+        //myCar1.drive();
+        //myCar1.brake();
     }
 }
