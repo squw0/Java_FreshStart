369aafce345e3b554fcfe977e7b021e2ce7d5f3d
Piotr Zawadzki <piotr.zawadzki0@tlen.pl>
2024-11-24 15:26:18 +0100

    added 4

---
+++ 4/Car.java
@@ -0,0 +1,10 @@
+public class Car extends Vehicle{
+    
+    int wheels = 4;
+    int doors = 4;
+    String name;
+
+    Car(String name){
+        this.name = name;
+    }
+}
--- 4/bObjectPassing.java
+++ 4/bObjectPassing.java
@@ -1,3 +1,12 @@
 public class bObjectPassing {
-    
+    public static void main(String[] args) {
+        
+        Garage garage = new Garage();
+
+        Car car1 = new Car("BMW");
+        Car car2 = new Car("Tesla");
+
+        garage.park(car1);
+        garage.park(car2);
+    }
 }
