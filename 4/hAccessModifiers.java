31035ea37f3d15a0b1cbb282456d8f49708907ad
Piotr Zawadzki <piotr.zawadzki0@tlen.pl>
2024-11-24 18:01:46 +0100

    added 4

--- 4/hAccessModifiers.java
+++ 4/hAccessModifiers.java
@@ -1,3 +1,8 @@
 public class hAccessModifiers {
     
+    // Modifier    |  Class  |  Package  |  Subclass  |  World
+    // Public      |  Y      |  Y        |  Y         |  Y
+    // Protected   |  Y      |  Y        |  Y         |  N
+    // No Modifier |  Y      |  Y        |  N         |  N
+    // Private     |  Y      |  N        |  N         |  N
 }
--- 4/iEncapsulation.java
+++ 4/iEncapsulation.java
@@ -1,3 +1,9 @@
 public class iEncapsulation {
-    
+    public static void main(String[] args) {
+        
+        // Encapsulation = attributes of a class will be hidden or private,
+        //                 can be accessed only through methods (getters & setters)
+        //                 you should make attributes private if you don't have 
+        //                 a reason to make them public/protected
+    }
 }
