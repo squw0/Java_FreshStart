62818ddcfeb7e15aa7f590cfb7a6e7d941bc3f85
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-12-01 21:51:25 +0100

    added multithreading

---
+++ MultiThreading/daemon.java
@@ -0,0 +1,28 @@
+public class daemon {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class DaemonHelper implements Runnable{
+
+    @Override
+    public void run(){
+        int count = 0;
+        while(count < 500){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+            }
+            count++;
+            System.out.println("Daemon helper running");
+        }
+    }
+}
+
+class UserThreadHelper implements Runnable{
+    @Override
+    public void run(){
+        
+    }
+}
---
+++ MultiThreading/join.java
@@ -0,0 +1,22 @@
+public class join {
+    public static void main(String[] args) throws InterruptedException {
+        Thread one = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Thread one: "+i);
+            }
+        });
+
+        Thread two = new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                System.out.println("Thread two: "+i);
+            }
+        });
+
+        System.out.println("before executing the threads");
+        one.start();
+        two.start();
+        one.join();
+        two.join();
+        System.out.println("done executing the threads");
+    }
+}
